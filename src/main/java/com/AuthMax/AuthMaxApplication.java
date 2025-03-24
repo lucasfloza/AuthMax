483c357e818340109190981f871793e4b39acc8b
lucasfloza <lucasfsouza.l10@gmail.com>
2025-03-24 18:33:20 -0300

    First Commit

---
+++ src/main/java/com/AuthMax/AuthMaxApplication.java
@@ -0,0 +1,13 @@
+package com.AuthMax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthMaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthMaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AuthMax/AuthMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AuthMax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
