356a01439400fd40927571b03b0f7e98a07e24a9
lucasfloza <lucasfsouza.l10@gmail.com>
2025-04-12 17:59:37 -0300

    feat(user): create model

---
+++ src/main/java/com/AuthMax/model/User.java
@@ -0,0 +1,44 @@
+package com.AuthMax.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(name = "created_at", updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    // OAuth2
+    private String oauthProvider;
+    private String oauthId;
+
+    public enum Role {
+        USER, ADMIN
+    }
+}
